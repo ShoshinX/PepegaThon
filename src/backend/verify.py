3269cbc1224d9fd7388ffa21608d7a31c71ca7f5
kyoukaya <kyoukaya@gmail.com>
2019-07-27 23:58:24 +1000

    Verify

--- src/backend/verify.py
+++ src/backend/verify.py
@@ -1,23 +1,13 @@
-def verify_sign(public_key, data):
-    '''
-    Verifies with a public key from whom the data came that it was indeed 
-    signed by their private key
-    param: public_key
-    param: signature String signature to be verified
-    return: Boolean. True if the signature is valid; False otherwise. 
-    '''
-    from Crypto.PublicKey import RSA 
-    from Crypto.Signature import PKCS1_v1_5 
-    from Crypto.Hash import SHA256 
-    from base64 import b64decode 
-    pub_key = public_key
-    rsakey = RSA.importKey(pub_key) 
-    signer = PKCS1_v1_5.new(rsakey) 
-    digest = SHA256.new() 
-    # Assumes the data is base64 encoded to begin with
-    digest.update(b64decode(data))
-    if signer.verify(digest, b64decode(public_key)):
-        return True
-    return False
+from base64 import b64decode, b64encode
+import ecdsa
 
-#given up on PGP
+
+def verify_sign(public_key, cleartext, encrypted):
+    """
+    public_key and encrypted are base64 encoded strings.
+    encrypted is the entire cleartext string but encrypted with
+    the user's private_key.
+    return: Boolean. True if the signature is valid; False otherwise. 
+    """
+    vk = ecdsa.VerifyingKey.from_string(b64decode(public_key.encode()))
+    return vk.verify(b64encode(encrypted), cleartext.encode())
